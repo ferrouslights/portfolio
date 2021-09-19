9d771a452c6a6148f74dc66d8f7efc63e2e57ef0
Benjamin Bridges <benjaminbridges.wayne@gmail.com>
2021-09-19 08:21:12 -0500

    Fixed infosection links

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 //this is default a react scroll link
 import styled from "styled-components";
-import { Link } from "react-scroll";
+import { Link } from "react-router-dom";
 
 export const Button = styled(Link)`
     border-radius: 50px;
@@ -16,6 +16,7 @@ export const Button = styled(Link)`
     justify-content: center;
     align-items: center;
     transition: all 0.2s ease-in-out;
+    text-decoration: none;
 
     @media (hover: hover) {
         &:hover{
